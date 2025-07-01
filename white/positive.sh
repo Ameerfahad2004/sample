74085c5900955c649eb13b45d5fa803aaad9f3d4
Ameerfahad2004 <fahadameer833@gmail.com>
2025-07-01 10:11:59 +0530

    added

---
+++ white/positive.sh
@@ -0,0 +1,13 @@
+echo "enter the first number"
+read num1
+echo "enter the second number"
+read num2
+if [ "$num1" -gt "$num2" ];
+then
+	echo "$num1 is greater than $num2"
+elif [ "$num1" -lt "$num2" ];
+then
+	echo "$num1 is less than $num2"
+else 
+	echo "both numbers are equal"
+fi
